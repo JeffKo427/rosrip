b2bd95bd9b69ac4f0674621416e4d94aa23c22ab
Jeff <JeffKo427@gmail.com>
2017-11-08 15:41:52 -0600

    Refactored code to comply with PEP8 style guide.

--- rosrip.py
+++ rosrip.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python
+"""Standalone script for extracting images from rosbagged video feeds.
+
+Parameters are set within the script itself, and must be the same for all
+ROSbags in the specified directory.
+
+Example:
+    [bag directory] is the directory your .bag files are in.
+    [image directory] is the directory you would like image files written to.
+    Both paths should be absolute.
+
+        $ python rosrip.py [bag directory] [image directory]
+
+Attributes:
+    FEED_TOPIC (string): The name of the ROS topic over which the images you
+        would like to extract were broadcast in the .bag file(s).
+    COMPRESSED (boolean): Whether the images were broadcast as CompressImage
+        messages instead of Image messages.
+    SAMPLING_FREQUENCY (int): How often you would like to sample a frame, in
+        seconds. Technically an interval rather than a frequency.
+    PREFIX (string): String prepended to all written image file names.
+    SUFFIX (string): String appended to all written image file names.
+    EXTENSION (string): Extension for the image format you would like,
+        typically '.png' or '.jpg'/'.jpeg'.
+    ROSBAG_DIR (string): Absolute path to the directory your ROS bags are in.
+    IMAGE_DIR (string): Absolute path to the directory images should be
+        written to.
+
+Todo:
+    * Accept relative or absolute paths.
+    * Intelligently determine COMPRESSED parameter from bag info.
+    * Specify other parameters in .yaml file, not in code.
+
+"""
+
+
 import rosbag
 from sensor_msgs.msg import Image
 import cv2
@@ -24,7 +59,10 @@ if not IMAGE_DIR.endswith('/'):
 
 BRIDGE = CvBridge()
 def write_image_to_file(data):
-    """docstring"""
+    """
+    Writes a sensor_msgs/Image to an image file.
+    Encoding is specified by global parameters above.
+    """
     try:
         if COMPRESSED:
             image = BRIDGE.compressed_imgmsg_to_cv2(data, "bgr8")
